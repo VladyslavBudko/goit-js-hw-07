8aeee6e0b48fb71f03e07c13cce20b1764cc8edd
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-28 02:16:36 +0300

    Добавил запрет перехода по клику

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,12 +6,17 @@ console.log(galleryItems);
 const galleryEl = document.querySelector("[class=gallery]");
 
 function createListGalleryItem(items) {
-  return items.map( item => `
-  <img class='gallery__item'
-  src = "${item.preview}"
-  alt = "${item.description}"
-  width = 340px
-   />`
+  return items
+    .map(
+      (item) => `
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>`
     )
     .join("");
 }
@@ -19,30 +24,27 @@ function createListGalleryItem(items) {
 const listGalleryItem = createListGalleryItem(galleryItems);
 galleryEl.innerHTML = listGalleryItem;
 
-// v2
+// v2 для тренировки
 
 // function createListGalleryItem(items) {
 //   const elements = [];
 
 //   items.map((item) => {
 //     const imgEl = document.createElement("img");
-//     imgEl.classList.add("gallery__item");
-
+//     imgEl.classList.add("gallery__image");
 //     imgEl.setAttribute("src", item.preview);
 //     imgEl.setAttribute("alt", item.description);
 
 //     elements.push(imgEl);
 //   });
-
 //   galleryEl.append(...elements);
 // }
 
-
-galleryEl.addEventListener('click', onClickGalleryItem);
+galleryEl.addEventListener("click", onClickGalleryItem);
 
 function onClickGalleryItem(event) {
-    if (event.target.nodeName !== "IMG") {
-        return;
-    }
-    console.log(event.target.nodeName);
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
 }
