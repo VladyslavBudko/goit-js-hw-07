703ea18409745c83ab9bf02978f7dc180a0ec39d
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-31 17:04:33 +0300

    2 подключил библиотеку simple-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector("[class=gallery]");
+
+function createListGalleryItem(items) {
+  return items
+    .map(
+      (item) => `
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>`
+    )
+    .join("");
+}
+
+const listGalleryItem = createListGalleryItem(galleryItems);
+galleryEl.innerHTML = listGalleryItem;
+
+galleryEl.addEventListener("click", onClickGalleryItem);
+
+function onClickGalleryItem(event) {
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  event.preventDefault();
+//   console.log(event.target);
+
+  showModalImg(event.target);
+}
+//   Modal from "basiclightbox";
+
+function showModalImg(eventTarget) {
+  const instance = basicLightbox.create(`
+    <img src="${eventTarget.dataset.source}" >
+`);
+
+  instance.show();
+
+}
+
